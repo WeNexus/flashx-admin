4d4cd039586bf06a8b12e6e68c6e17db3c5cdb00
sifat-wenexus <sifat@wenexus.io>
2026-08-16 14:24:14 +0600

    fix(config): update BASE_URL to point to the sales discounts API

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL ="https://pointed-vista-beth-manhattan.trycloudflare.com"
+// export const BASE_URL ="https://pointed-vista-beth-manhattan.trycloudflare.com"
 
-// export const BASE_URL = "https://sales-discounts.wenexus.io";
+export const BASE_URL = "https://sales-discounts.wenexus.io";
